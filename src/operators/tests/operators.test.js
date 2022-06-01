96a64f6d9e2f7da283fd6b743bba531d8b9aad85
Maria Luísa Campos <mluisa.cfl@gmail.com>
2022-06-01 10:07:58 -0300

    mlc: add 3 tests to operators functions

---
+++ src/operators/tests/operators.test.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+
+const {
+  describe, it, expect, beforeAll, afterAll, beforeEach,
+} = require('@jest/globals');
+const supertest = require('supertest');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const { createServer } = require('../../server');
+
+const app = createServer();
+
+describe('Operators Routes', () => {
+  const mockOperator = {
+    ansRegister: "12597654863",
+    cnpj: "37914667000693",
+    corporateName: "Serviços LTDA",
+    fantasyName: "Serviços shows",
+    modality: "Saúde",
+    adress: "Rua da Saúde, 50, sala 1015",
+    district: "Bairro",
+    city: "Recife",
+    state: "PE",
+    cep: "50070110",
+    telephone: "81996096812",
+    fax: "12345698",
+    email: "empresa@email.com",
+    representative: "Maria Luísa",
+    representativeJob: "Gerente de Vendas"
+    };
+  
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri());
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+  });
+
+  beforeEach(async () => {
+    await supertest(app)
+      .post('/api/operators')
+      .send(mockOperator);
+  })
+
+  it('should register a new operator by request body', async () => {
+    const mockOperatorLocal = {
+      ...mockOperator,
+      ansRegister: '176259846'
+    }
+
+    const { statusCode, body } = await supertest(app)
+      .post('/api/operators')
+      .send(mockOperatorLocal);
+    
+    expect(statusCode).toBe(200);
+    expect(body).toEqual({
+      message: "Operator registered.",
+      operator: {
+        __v: expect.any(Number),
+        _id: expect.any(String),
+        ansRegister: expect.any(String),
+        cnpj: expect.any(String),
+        corporateName: expect.any(String),
+        fantasyName: expect.any(String),
+        modality: expect.any(String),
+        adress: expect.any(String),
+        createdAt: expect.any(String),
+        district: expect.any(String),
+        city: expect.any(String),
+        state: expect.any(String),
+        cep: expect.any(String),
+        telephone: expect.any(String),
+        fax: expect.any(String),
+        email: expect.any(String),
+        representative: expect.any(String),
+        representativeJob: expect.any(String)
+      }
+    });
+  });
+
+  it('should not register operator with the same ans register', async () => {
+    const { statusCode, body } = await supertest(app)
+      .post('/api/operators')
+      .send(mockOperator);
+
+    expect(statusCode).toBe(400);
+    expect(body).toEqual({ message: `ANS Register ${mockOperator.ansRegister} already exists` });
+  });
+
+  it('should register all operators from csv file', async() => {
+    const { statusCode } = await supertest(app)
+      .post('/api/operators/csv')
+
+    expect(statusCode).toBe(200);
+  })
+});
